e9d7e041be07ee341f2f7038c3ad1a90212c54d3
Fernando Branco <71955135+favanso@users.noreply.github.com>
2021-04-05 01:22:43 -0400

    Final Project

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -8,53 +8,47 @@
 using namespace std;
 
 int main() {
-	ifstream file;
+	ifstream infile;
 	string word;
-	vector<string> wordVector;
-
-
-	file.open("ships.txt");
-	if (file.is_open()) {
-		while (file >> word) {
-			wordVector.push_back(word);
+	string shipName;
+	string captainName;
+	int shieldStrength;
+	int numPhasers;
+	int numPhotons;
+	vector<Spaceship*> myVector;
+	
+	infile.open("ships.txt");
+	if (infile.is_open()) {
+		while (!infile.eof())
+		{
+		infile >> shipName >> captainName >> shieldStrength >> numPhasers >> numPhotons;
+		
+		Spaceship* tempPtr = new Spaceship(shipName, captainName, shieldStrength, numPhasers, numPhotons);
+		
+		myVector.push_back(tempPtr);
+		
 		}
+
 		file.close();
+
 	}
 	else {
 		cout << "failed to open the file." << endl;
 	}
-
-	Spaceship *spacePoint;
-	Spaceship *spacePoint2;
-	Spaceship *spacePoint3;
 	
+	int size = myVector.size();
+	for (int i = 0; i < size; i++) {
 
-	spacePoint = new Spaceship (wordVector[0], wordVector[1], stoi(wordVector[2]), stoi(wordVector[3]), stoi(wordVector[4]));
-	spacePoint2 = new Spaceship(wordVector[5], wordVector[6], stoi(wordVector[7]), stoi(wordVector[8]), stoi(wordVector[9]));
-	spacePoint3 = new Spaceship(wordVector[10], wordVector[11], stoi(wordVector[12]), stoi(wordVector[13]), stoi(wordVector[14]));
-
-	vector<Spaceship*> spaceshipVec;
-	spaceshipVec.push_back(spacePoint);
-	spaceshipVec.push_back(spacePoint2);
-	spaceshipVec.push_back(spacePoint3);
-
-
-	for (Spaceship* space : spaceshipVec){
-
-		cout << "Starship " << space->getShipName() << " is captained by " << space->getCaptainName() <<
-		". It has shield strenght of " << space->getShieldStrenght() << ", " << space->getNumPhasers() << " phasers, and "
-		<< space->getNumPhotons() << " photon torpedoes." << endl;
+		
+		myVector[i]->printShip();
 		
 		int hit;
 		hit = rand() % 100;
-		space->takeDamage(hit);
-		
+		myVector[i]->takeDamage(hit);
+		delete myVector[i];
 	}
-	
-	delete spacePoint;
-	delete spacePoint2;
-	delete spacePoint3;
 
-	
+
+
 	return 0;
 }
